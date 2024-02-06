c21b53363e5bb74d709ab2863dfb8edf546db4da
AidenAC <aidenc@umich.edu>
2024-02-06 15:07:57 -0500

    added events page

--- Pages/views.py
+++ Pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def events(request):
+    eventList = Event.objects.all()
+    return render(request, 'events.html', {'eventList': eventList})
