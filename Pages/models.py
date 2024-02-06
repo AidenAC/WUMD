900431166636570302116c3096e06a1854a2b9e6
AidenAC <aidenc@umich.edu>
2024-02-06 14:44:43 -0500

    added event table to db

--- Pages/models.py
+++ Pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Event (models.Model):
+    name = models.CharField('Event Name', max_length=50)
+    location = models.CharField('Location', max_length=100)
+    date = models.DateField('Event Date', primary_key=True)
+    time = models.TimeField('Start Time')
+
+    def __str__(self):
+        return self.name
