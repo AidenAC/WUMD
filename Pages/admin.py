f8c6076b5b4b10d2801cab7ca44cfaaa3b02ed08
AidenAC <aidenc@umich.edu>
2024-02-06 18:32:00 -0500

    added all tables to db

--- Pages/admin.py
+++ Pages/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Event
+from .models import Member, EBoard, Show, Event
 
 # Register your models here.
+admin.site.register(Member)
+admin.site.register(EBoard)
+admin.site.register(Show)
 admin.site.register(Event)
--- Pages/models.py
+++ Pages/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Member (models.Model):
+    name = models.CharField('Member Name', max_length=30)
+    memberNumber = models.IntegerField(primary_key=True)
+    classLevel = models.CharField('Class', max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+class EBoard (models.Model):
+    position = models.CharField('Postion', max_length=20, primary_key=True)
+    member = models.ForeignKey(Member, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.position
+    
+class Show (models.Model):
+    name = models.CharField('Show Name', max_length=50, primary_key=True)
+    host = models.ForeignKey(Member, blank=True, null=True, on_delete=models.CASCADE)
+    day = models.CharField('Day on Air', max_length=10, null=True)
+    time = models.TimeField('Air Time', null=True)
+
+    def __str__(self):
+        return self.name
+
 class Event (models.Model):
     name = models.CharField('Event Name', max_length=50)
     location = models.CharField('Location', max_length=100)
