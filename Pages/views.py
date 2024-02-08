e7b2c2299c8dc3064184548c6f8e37188e31fb29
AidenAC <aidenc@umich.edu>
2024-02-07 23:33:35 -0500

    refactored templates path

--- Pages/views.py
+++ Pages/views.py
@@ -3,16 +3,16 @@ from .models import Show, Event
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'Pages/home.html', {})
 
 def shows(request):
     showList = Show.objects.all()
-    return render(request, 'shows.html', {'showList': showList})
+    return render(request, 'Pages/shows.html', {'showList': showList})
 
 def events(request):
     eventList = Event.objects.all()
-    return render(request, 'events.html', {'eventList': eventList})
+    return render(request, 'Pages/events.html', {'eventList': eventList})
 
 def eventInfo(request, event_pk):
     event = Event.objects.get(pk = event_pk)
-    return render(request, 'event-info.html', {'event': event})
+    return render(request, 'Pages/event-info.html', {'event': event})
